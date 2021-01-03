afb8ec7a883029ecccd7f71f8e8b309ebccc1bd5
Ilya <hairais@mail.ru>
2021-01-03 20:24:59 +0700

    UPDATE v0.1
    Добавлена возможность полноценно играть, но требуются доработки
    Оптимизация кода

--- main.py
+++ main.py
@@ -65,9 +65,8 @@ class MainMatches(QtWidgets.QMainWindow, main_layout.Ui_MainWindow):
         self.author2_line.setText(" Вы играете против игрока")
         self.author1_line.setStyleSheet('color: rgb(245, 245, 245)')
 
-        self._timer.singleShot(4000, lambda: self.author1_line.setText(" Чтобы начать играть,"))
-        self._timer.singleShot(4000, lambda: self.author2_line.setText(" Нажмите кнопку Продолжить"))
-        self._timer.singleShot(4500, lambda: self.continue_btn.setEnabled(True))
+        self._timer.singleShot(2000, lambda: self.first_launch())
+        self._timer.singleShot(2000, lambda: self.continue_btn.setEnabled(True))
 
         self.continue_btn.clicked.connect(self.first_launch)
 
@@ -87,111 +86,96 @@ class MainMatches(QtWidgets.QMainWindow, main_layout.Ui_MainWindow):
         print("first launch")
 
         self.continue_btn.clicked.disconnect(self.first_launch)
-        self.continue_btn.clicked.connect(self.manager_turns)
-
-        self.manager_turns()
+        self.continue_btn.clicked.connect(self.player1_turn)
 
     def manager_turns(self):
         print("manager turns")
 
-        back = self.player1_turn()
-        if back == 1:
+        self.back = self.player1_turn()
+        if self.back == 1:
             self._timer.singleShot(1000, lambda: self.player2_turn())
 
     def player1_turn(self):
         print("Ход 1")
-        try:
-            # ход первого игрока
-            self.author1_line.setText("Ходит первый игрок")
-            self._timer.singleShot(2000, lambda: self.player_line.setPlaceholderText(
-                "Первый игрок, сколько Вы хотите взять спичек? (от 1 до 3)"))
+        if self.quantity_matches > 0:
+            try:
+                # ход первого игрока
+                self.author1_line.setText(" Ходит первый игрок")
+                self.author2_line.clear()
+                self.player_line.setPlaceholderText("Первый игрок, сколько Вы хотите взять спичек? (от 1 до 3)")
 
-            self.player1_take_matches = int(self.player_line.text())
-            if self.player1_take_matches > 3 or self.player1_take_matches < 1:
-                raise ValueError
+                self.player1_take_matches = int(self.player_line.text())
+                if self.player1_take_matches > 3 or self.player1_take_matches < 1:
+                    raise ValueError
 
-            self.check_ending()
+                else:
+                    self.check_ending()
 
-            self.quantity_matches = self.quantity_matches - self.player1_take_matches
-            self.author2_line.setText(f" Ход сделан - {self.player1_take_matches} спичк{self.ending}")
-            self.player_line.setDisabled(True)
-            self.continue_btn.setDisabled(True)
-            return 1
+                    self.quantity_matches = self.quantity_matches - self.player1_take_matches
+                    # self.author2_line.setText(f" Ход сделан - {self.player1_take_matches} спичк{self.ending}")
+                    self.player_line.clear()
+                    self.player_line.setDisabled(True)
+                    self.continue_btn.setDisabled(True)
 
-        except ValueError:
-            self.player_line.setText("Введённые данные неправильны")
-            self.player_line.setStyleSheet('color: red')
-            self._timer.singleShot(2000, lambda: self.player_line.clear())
-            self._timer.singleShot(2000, lambda: self.player_line.setStyleSheet('color: black'))
+                    self.continue_btn.clicked.disconnect(self.player1_turn)
+                    self.continue_btn.clicked.connect(self.player2_turn)
+                    self.player2_turn()
+
+            except ValueError:
+                self.player_line.setText("Введённые данные неправильны")
+                self.player_line.setStyleSheet('color: red')
+                self._timer.singleShot(2000, lambda: self.player_line.clear())
+                self._timer.singleShot(2000, lambda: self.player_line.setStyleSheet('color: black'))
+
+            finally:
+                print(self.quantity_matches)
+
+        else:
+            self.author1_line.setText(" Выиграл второй игрок")
+            self.author2_line.setText(" Хотите начать заново?")
+            self.player_line.setPlaceholderText("")
 
     def player2_turn(self):
         print("Ход 2")
-        try:
-            self.author1_line.setText("Ходит второй игрок")
-            self._timer.singleShot(1000, lambda: self.player_line.setEnabled(True))
-            self._timer.singleShot(1000, lambda: self.continue_btn.setEnabled(True))
+        if self.quantity_matches > 0:
+            try:
+                self.author1_line.setText(" Ходит второй игрок")
+                self._timer.singleShot(1000, lambda: self.player_line.setEnabled(True))
+                self._timer.singleShot(1000, lambda: self.continue_btn.setEnabled(True))
 
-            # ход второго игрока
-            self._timer.singleShot(2000, lambda: self.player_line.setPlaceholderText(
-                "Второй игрок, сколько Вы хотите взять спичек? (от 1 до 3)"))
+                # ход второго игрока
+                self.player_line.setPlaceholderText("Второй игрок, сколько Вы хотите взять спичек? (от 1 до 3)")
 
-            self.player2_take_matches = int(self.player_line.text())
-            if self.player2_take_matches > 3 or self.player2_take_matches < 1:
-                raise ValueError
+                self.player2_take_matches = int(self.player_line.text())
+                if self.player2_take_matches > 3 or self.player2_take_matches < 1:
+                    raise ValueError
 
-            self.check_ending()
+                else:
+                    self.check_ending()
 
-            self.quantity_matches = self.quantity_matches - self.player2_take_matches
-            self.author2_line.setText(f" Ход сделан - {self.player2_take_matches} спичк{self.ending}")
-            self.player_line.setDisabled(True)
-            self.continue_btn.setDisabled(True)
+                    self.quantity_matches = self.quantity_matches - self.player2_take_matches
+                    # self.author2_line.setText(f" Ход сделан - {self.player2_take_matches} спичк{self.ending}")
+                    self.player_line.clear()
+                    self.player_line.setDisabled(True)
+                    self.continue_btn.setDisabled(True)
 
-        except ValueError:
-            self.player_line.setText("Введённые данные неправильны")
-            self.player_line.setStyleSheet('color: red')
-            self._timer.singleShot(2000, lambda: self.player_line.clear())
-            self._timer.singleShot(2000, lambda: self.player_line.setStyleSheet('color: black'))
+                    self.continue_btn.clicked.disconnect(self.player2_turn)
+                    self.continue_btn.clicked.connect(self.player1_turn)
+                    self.player1_turn()
 
-    # диапазон количества спичек
-    def range_take_matches(self, player):
-        if (self.player1_take_matches > 3 or self.player1_take_matches < 1) or \
-                (self.player2_take_matches > 3 or self.player2_take_matches < 1):
-            print("Неправильно, попробуйте ещё раз")
-            if player == 1:
-                self.player1_take_matches = int(
-                    input("Первый игрок, сколько Вы хотите взять спичек? (от 1 до 3): "))
-            if player == 2:
-                self.player2_take_matches = int(
-                    input("Второй игрок, сколько Вы хотите взять спичек? (от 1 до 3): "))
+            except ValueError:
+                self.player_line.setText("Введённые данные неправильны")
+                self.player_line.setStyleSheet('color: red')
+                self._timer.singleShot(2000, lambda: self.player_line.clear())
+                self._timer.singleShot(2000, lambda: self.player_line.setStyleSheet('color: black'))
 
-    # игра против ИИ
-    def vs_ai(self):
-        pass
+            finally:
+                print(self.quantity_matches)
 
-    # игра против игрока
-    def vs_player(self):
-        print("==== Вы выбрали режим игры -Против игрока- ====")
-
-        vs_player_running = True
-        while vs_player_running:
-            print("==== Ходит первый игрок ====")
-            self.player1_take_matches = int(input("Первый игрок, сколько Вы хотите взять спичек? (от 1 до 3): "))
-            self.range_take_matches(1)
-            self.quantity_matches = self.quantity_matches - self.player1_take_matches
-            if self.quantity_matches <= 0:
-                print("Проигрышь 1")
-                break
-            print("==== Ход сделан ====")
-            print(f"Количество спичек - {self.quantity_matches}")
-            print("==== Ходит второй игрок ====")
-            self.player2_take_matches = int(input("Второй игрок, сколько Вы хотите взять спичек? (от 1 до 3): "))
-            self.range_take_matches(2)
-            self.quantity_matches = self.quantity_matches - self.player2_take_matches
-            if self.quantity_matches <= 0:
-                print("Проигрышь 2")
-                break
-            print("==== Ход сделан ====")
-            print(f"Количество спичек - {self.quantity_matches}")
+        else:
+            self.author1_line.setText(" Выиграл первый игрок")
+            self.author2_line.setText(" Хотите начать заново?")
+            self.player_line.setPlaceholderText("")
 
     def check_ending(self):
         if self.player1_take_matches == 1:
